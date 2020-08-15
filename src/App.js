eebf55d878731793cf69454ddfa7cbe241f80daf
arturbrd <artur.ab2002@gmail.com>
2020-08-15 15:27:10 +0200

    minor change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import { Header } from "./containers/header";
-import { AddTaskButton } from "./containers/addTaskButton";
+import { Header } from "./components/header";
+import { AddTaskButton } from "./components/addTaskButton";
 
 
 
